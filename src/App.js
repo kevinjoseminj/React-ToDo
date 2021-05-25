8bb1bbcfd3e3f3a81092c456c33d1a10934a87a7
Kevin Jose Minj <kevinjoseminj98@gmail.com>
2021-05-25 16:57:50 +0530

    task adding component defined

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [toDos,setToDos]=useState([])
+  const [toDo,setToDo]=useState('')
   return (
     <div className="app">
       <div className="mainHeading">
@@ -12,20 +14,33 @@ function App() {
         <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
       </div>
       <div className="input">
-        <input type="text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
+        <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>setToDos([...toDos,{id:Date.now() ,text: toDo,status:false}])} className="fas fa-plus"></i>
       </div>
       <div className="todos">
-        <div className="todo">
+        { toDos.map((obj)=>{
+        return ( <div className="todo">
           <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>Rect tutorial</p>
+            <input onChange={(e)=>{ 
+              console.log(e.target.checked);
+              console.log(obj);
+              setToDos(toDos.filter(obj2=>{
+                if(obj2.id===obj.id){
+                  obj.status=e.target.checked
+                }
+                return obj2
+              }))
+
+            }} value={obj.status} type="checkbox" name="" id="" />
+            <p>{obj.text}</p>
           </div>
           <div className="right">
             <i className="fas fa-times"></i>
           </div>
         </div>
-      </div>
+        )
+      }) }
+      </div>   
     </div>
   );
 }
